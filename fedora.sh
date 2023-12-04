485525f69d3ae3084a8adfa8bedcab9bb53d8527
Patrick Girardi <111985260+isTrick@users.noreply.github.com>
2023-12-04 14:23:02 -0300

    Create fedora.sh

---
+++ fedora.sh
@@ -0,0 +1,38 @@
+###################################
+# post-install scripts for Fedora #
+###################################
+
+# remove random apps
+sudo dnf remove yelp gnome-maps mediawriter cheese gnome-tour rhythmbox simple-scan gnome-connections
+
+# remove extensoes do sistema
+sudo dnf remove gnome-shell-extension-background-logo
+sudo dnf remove gnome-classic-session
+
+# instala extensoes do gnome basicas
+sudo dnf install gnome-shell-extension-appindicator gnome-shell-extension-caffeine gnome-shell-extension-dash-to-dock gnome-shell-extension-gsconnect gnome-shell-extension-pop-shell gnome-shell-extension-user-theme
+sudo dnf remove gnome-extensions-app
+
+# instala apps basicos
+sudo dnf install gnome-tweaks neofetch
+
+# instala alguns flatpaks
+sudo flatpak install com.mattjakeman.ExtensionManager
+sudo flatpak install com.spotify.Client
+
+# customizaçao basica dos temas
+
+## tema adw-gtk
+sudo dnf install adw-gtk3-theme
+gsettings set org.gnome.desktop.interface gtk-theme "Adw-gtk3-dark"
+
+## cursor bibata translucent
+git clone https://github.com/Silicasandwhich/Bibata_Cursor_Translucent
+cd Bibata_Cursor_Translucent
+./Installer_Bibata.sh
+cd
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata_Ghost'
+rm -r Bibata_Cursor_Translucent
+
+# altera o layout do touchpad
+gsettings set org.gnome.desktop.peripherals.touchpad click-method 'areas'
