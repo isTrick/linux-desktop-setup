7ed9a9a8c658e85110706b69b168b3261fafb124
Patrick Girardi <111985260+isTrick@users.noreply.github.com>
2023-12-06 10:12:04 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -2,21 +2,25 @@
 # post-install scripts for Fedora #
 ###################################
 
-# remove random apps
+# remove random packages
 sudo dnf remove yelp gnome-maps mediawriter cheese gnome-tour rhythmbox simple-scan gnome-connections
 
 # remove extensoes do sistema
-sudo dnf remove gnome-shell-extension-background-logo gnome-classic-session
+sudo dnf remove gnome-shell-extension-background-logo gnome-classic-session gnome-extensions-app
 
 # instala extensoes do gnome basicas
-sudo dnf install gnome-shell-extension-appindicator gnome-shell-extension-caffeine gnome-shell-extension-dash-to-dock gnome-shell-extension-gsconnect gnome-shell-extension-pop-shell gnome-shell-extension-user-theme
-sudo dnf remove gnome-extensions-app
+sudo dnf install gnome-shell-extension-appindicator gnome-shell-extension-caffeine gnome-shell-extension-dash-to-dock gnome-shell-extension-user-theme
 
 # instala apps basicos
-sudo dnf install google-chrome gnome-tweaks htop nvidia-driver
+sudo dnf install google-chrome-stable gnome-tweaks nvidia-driver pycharm-community cava epiphany
 
 # instala alguns flatpaks
-sudo flatpak install com.mattjakeman.ExtensionManager com.Spotify.Client it.mijorus.gearlever
+flatpak install com.mattjakeman.ExtensionManager it.mijorus.gearlever com.raggesilver.BlackBox # configurações
+flatpak install de.haeckerfelix.Fragments com.github.tenderowl.frog com.github.finefindus.eyedropper # utilitários
+flatpak install com.Spotify.Client io.bassi.Amberol com.obsproject.Studio # mídia
+flatpak install io.gitlab.idevecore.Pomodoro com.todoist.Todoist # produtividade
+flatpak install org.ferdium.Ferdium io.github.mimbrero.WhatsAppDesktop org.telegram.desktop com.protonvpn.www  # internet
+flatpak install io.podman_desktop.PodmanDesktop org.gnome.Builder # desenvolvimento
 
 # customizaçao basica dos temas
 
@@ -26,11 +30,13 @@ gsettings set org.gnome.desktop.interface gtk-theme "Adw-gtk3-dark"
 
 ## cursor bibata translucent
 git clone https://github.com/Silicasandwhich/Bibata_Cursor_Translucent
-cd Bibata_Cursor_Translucent
-sudo ./Installer_Bibata.sh
-cd
+sudo ./Bibata_Cursor_Translucent/Installer_Bibata.sh
 gsettings set org.gnome.desktop.interface cursor-theme 'Bibata_Ghost'
 rm -r Bibata_Cursor_Translucent
 
-# altera o layout do touchpad
-gsettings set org.gnome.desktop.peripherals.touchpad click-method 'areas'
+## tema de icones MoreWaita
+git clone https://github.com/somepaulo/MoreWaita.git
+sudo ./MoreWaita/install.sh
+gsettings set org.gnome.desktop.interface icon-theme 'MoreWaita'
+rm -r MoreWaita
+
